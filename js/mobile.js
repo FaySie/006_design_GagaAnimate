cbff57484cab7f14bd9200ee2568f97126f11e67
FaySie <sel12342001@gmail.com>
2017-05-07 00:17:11 +0800

    add menu_active & section1 bug

--- js/mobile.js
+++ js/mobile.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	    $(".menu_toggle").click(function(){
-	    	if($(this).hasClass("clicked")) {             
-	    		$("#menu_area").removeClass("menu_animate");             
-	    		$(this).removeClass("clicked");         
-	    	} else {             
-	    		$("#menu_area").addClass("menu_animate");            
-	    		$(this).addClass("clicked");         
-	    	}
-		});
+    $(".menu_toggle").click(function() {
+    	if($(this).hasClass("clicked")) {             
+    		$(".menu_area").removeClass("menu_animate");             
+    		$(this).removeClass("clicked");         
+    	} else {             
+    		$(".menu_area").addClass("menu_animate");            
+    		$(this).addClass("clicked");         
+    	}
+	});
 });
